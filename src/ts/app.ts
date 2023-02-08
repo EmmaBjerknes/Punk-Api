abc919adbba53b2d66aba829b3304d0540346e22
Emma Bjerknes <Bjerknes.Emma@gmail.com>
2023-02-08 16:15:39 +0100

    renaming and cleaning

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -3,13 +3,16 @@ const urlBase = "https://api.punkapi.com/v2/beers";
 import RootObject from './beer';
 
 const inputQueryString = document.querySelector("#query-string") as HTMLInputElement;
-const buttonSearch = document.querySelector("#search-button") as HTMLButtonElement;
-const buttonRandFood = document.querySelector("#random-food-button") as HTMLButtonElement;
-const wrapper = document.querySelector("#content-box") as HTMLElement;
-const selectBeer = document.querySelector("#beerName") as HTMLSelectElement;
+const searchButton = document.querySelector("#search-button") as HTMLButtonElement;
+const randFoodButton = document.querySelector("#random-food-button") as HTMLButtonElement;
+const cardWrapper = document.querySelector("#content-box") as HTMLElement;
+const beerSelect = document.querySelector("#beerName") as HTMLSelectElement;
 const randomFoodBox = document.querySelector("#random-food") as HTMLTableSectionElement;
 const selectedPageList = document.querySelector("#pageList") as HTMLSelectElement;
 const randomFoodParagraf = document.createElement("p");
+const savedBeerBtn = document.querySelector(".fav-btn") as HTMLButtonElement;
+const savedBeerBox = document.querySelector("#fav-list") as HTMLDivElement;
+const savedBeerUl= document.querySelector(".fav-ul") as HTMLUListElement;
 
 /* === Save beer info === */ 
 interface Card {
@@ -22,15 +25,14 @@ interface Card {
 
 let likedArr:Card[] = []; 
 
-function pushit(x: string, y:string, e: string, r:string, f:string[]){
-    let derp: Card = {
-    name: x,
-    img: y,
-    tagline: e,
-    description: r,
-    food_pairing: f}
-    likedArr.push(derp);
-    console.log(likedArr)
+function pushIt(newName: string, newImg:string, newTagline: string, newDesc:string, newFood:string[]){
+    const newCard: Card = {
+    name: newName,
+    img: newImg,
+    tagline: newTagline,
+    description: newDesc,
+    food_pairing: newFood,}
+    likedArr.push(newCard);
 };
 
 
@@ -40,7 +42,6 @@ async function getBeerData(param:string, value:string){
     const data: RootObject[] = await response.json();
     if (data.length <= 0){
         alert("No food matches your search. Try: tips");
-
     }
     return data;
 };
@@ -50,6 +51,7 @@ function remakeString(str:string):string{
     const sliced = str.replace(" ", "_");
     return sliced
 };
+
 /* === Takes the user input when searching for food / click the random food option === */
 async function foodSearch(foodName:string){
 
@@ -61,10 +63,10 @@ async function foodSearch(foodName:string){
     });
 }
 
-let foodArr: string[]= [];
 /* === Create Cards that holds beer info === */
+let foodArr: string[]= [];
 function createCard(beer: RootObject[], o:number){ 
-    wrapper.innerHTML= "";
+    cardWrapper.innerHTML= "";
     for (let i = 0; i < o; i++) {
 
         foodArr = [];
@@ -76,8 +78,8 @@ function createCard(beer: RootObject[], o:number){
         const cardFood = document.createElement("p");
         const cardImg = document.createElement("img");
 
-        const saveBeer =document.createElement("button");
-        saveBeer.innerText = "Save";
+        const saveBeerBtn =document.createElement("button");
+        saveBeerBtn.innerText = "Save";
 
         if(beer[i].image_url === null){
             cardImg.alt = "Oh, there is no img to this beer"
@@ -93,22 +95,22 @@ function createCard(beer: RootObject[], o:number){
 
         // change to check length of food_pairing array
         const foodArrLength = beer[i].food_pairing.length;
-        console.log(foodArrLength);
+
         for(let x = 0; x < foodArrLength; x++){
             if(beer[i].food_pairing[x] === undefined){
-                console.log("Herp");
                 cardFood.innerHTML += "";
             }else{
                 cardFood.innerHTML += `${beer[i].food_pairing[x]} </br>`; 
                 foodArr.push(beer[i].food_pairing[x]);             
             }
         }
-        beerCard.append(cardHeader, saveBeer, cardTagline, cardDesc, cardFood, cardImg);      
-        wrapper.append(beerCard);
+        beerCard.append(cardHeader, saveBeerBtn, cardTagline, cardDesc, cardFood, cardImg);      
+        cardWrapper.append(beerCard);
  
-        saveBeer.addEventListener('click', (event) =>{
+        saveBeerBtn.addEventListener('click', (event) =>{
             event.preventDefault();
-            pushit(beer[i].name, beer[i].image_url, beer[i].tagline, beer[i].description, foodArr); 
+            pushIt(beer[i].name, beer[i].image_url, beer[i].tagline, beer[i].description, foodArr);
+            saveBeerBtn.style.display = "none"; 
         })
     }
 }
@@ -118,20 +120,20 @@ async function getNameOfBeer(x: string) {
     const response = await fetch(urlBase + x);
     const data: RootObject[] = await response.json();
 
-    selectBeer.innerHTML = `<option value="empty" selected disabled>--Please choose an option--</option>`;
+    beerSelect.innerHTML = `<option value="empty" selected disabled>--Please choose an option--</option>`;
 
     data.forEach((element) => { 
         let optionCategory = document.createElement("option");
         optionCategory.innerHTML = element.name;
-        selectBeer.append(optionCategory);
+        beerSelect.append(optionCategory);
     });
 }
 
 /* === EventListeners === */
 // free text search for food
-buttonSearch.addEventListener("click",async (event) => {
+searchButton.addEventListener("click",async (event) => {
     event.preventDefault();
-    wrapper.innerHTML= "";
+    cardWrapper.innerHTML= "";
     if(inputQueryString.value.length > 0){
         foodSearch(inputQueryString.value);
     }else{
@@ -140,9 +142,9 @@ buttonSearch.addEventListener("click",async (event) => {
 });
 
 // Dropdown that shows the name of the beers
-selectBeer.addEventListener("change", async(event)=>{
+beerSelect.addEventListener("change", async(event)=>{
     event.preventDefault();
-    getBeerData("?beer_name=", selectBeer.value).then((beer: RootObject[])=>{
+    getBeerData("?beer_name=", beerSelect.value).then((beer: RootObject[])=>{
         const arrLength : number = beer.length;
         createCard(beer, arrLength);
     });
@@ -151,12 +153,12 @@ selectBeer.addEventListener("change", async(event)=>{
 // Dropdown that toggle between the API-pages of all beers
 selectedPageList.addEventListener("change", async(event)=>{
     event.preventDefault();
-    selectBeer.innerHTML = "";
+    beerSelect.innerHTML = "";
     getNameOfBeer(selectedPageList.value);
 });
 
 // Gives a random food option 
-buttonRandFood.addEventListener("click", async (event)=>{
+randFoodButton.addEventListener("click", async (event)=>{
     event.preventDefault();
     let foodSugg: string;
 
@@ -167,7 +169,7 @@ buttonRandFood.addEventListener("click", async (event)=>{
             foodSugg = element.food_pairing[random];
         });
 
-        buttonRandFood.innerText = "Give me another one";
+        randFoodButton.innerText = "Give me another one";
         const yesBtn = document.createElement("button");
         yesBtn.innerText = "Yes!";
 
@@ -185,11 +187,13 @@ buttonRandFood.addEventListener("click", async (event)=>{
 
 // Start of showing saved beer 
 // remove -> remove from array Card
-const savedBeerBtn = document.querySelector(".fav-btn") as HTMLButtonElement;
-const savedBeerBox = document.querySelector("#fav-list") as HTMLDivElement;
-const savedBeerUl= document.querySelector(".fav-ul") as HTMLUListElement;
-
-savedBeerBox.style.display = "none";
+savedBeerBtn.addEventListener("click", async (event)=>{
+    event.preventDefault();
+    cardWrapper.innerHTML= "";
+    savedBeerUl.innerHTML = "";
+    savedBeerBox.style.display = "block";
+    showLikedList();
+}); 
 
 function showLikedList (){
     for(let i= 0; i< likedArr.length; i++){
@@ -236,21 +240,8 @@ function showLikedList (){
             });
         });
     }
-
 };
-
-savedBeerBtn.addEventListener("click", async (event)=>{
-    event.preventDefault();
-    wrapper.innerHTML= "";
-    savedBeerUl.innerHTML = "";
-    savedBeerBox.style.display = "block";
-    showLikedList();
-}); 
 
 
 /* === Start of program === */
 getNameOfBeer("?page=1&per_page=65");
-
-
-// When cards loads -> needs a output  
-
